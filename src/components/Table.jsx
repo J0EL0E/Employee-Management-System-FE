c1a564903adfa3ebe46e0a0841ac3afd0c6623ff
Jose Luis Salvador <jose.salvador@thynker.tech>
2025-11-28 14:46:31 +0800

    added a tbody tag

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -9,25 +9,26 @@ const Table = () => {
 
   return (
     <table className='w-[90%] h-full border-gray-500 border-2 rounded-md px-[5%] mx-auto mt-10 mb-[10vh]'>
-
         <TableHeaders />
-        { currentEmployeeList ? currentEmployeeList.map(employee => 
-            <TableData 
-                key={employee.id} 
-                id={employee.id} 
-                photo_url={employee.photo_url} 
-                first_name={employee.first_name} 
-                last_name={employee.last_name} 
-                username={employee.username} 
-                country={employee.country} 
-                email_address={employee.email_address} 
-                account_type={employee.account_type}
-            /> 
-        ) : 
-        <div className='w-full h-full'>
-            <p>No Employee Data Found</p>
-        </div>
-        }
+        <tbody>
+            { currentEmployeeList ? currentEmployeeList.map(employee => 
+                <TableData 
+                    key={employee.id} 
+                    id={employee.id} 
+                    photo_url={employee.photo_url} 
+                    first_name={employee.first_name} 
+                    last_name={employee.last_name} 
+                    username={employee.username} 
+                    country={employee.country} 
+                    email_address={employee.email_address} 
+                    account_type={employee.account_type}
+                /> 
+            ) : 
+            <div className='w-full h-full'>
+                <p>No Employee Data Found</p>
+            </div>
+            }
+        </tbody>
     </table>
   )
 }
